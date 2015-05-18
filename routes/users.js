507425676af979e1f6c12f97b856dd76411b6aa7
chrisjyap <chrisjyap@gmail.com>
2015-05-17 19:16:53 -0700

    first request to get ID

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,12 @@
 var express = require('express');
+var config = require('../config.json');
+var request = require('request');
 var router = express.Router();
+
+const baseURL = 'https://na.api.pvp.net/api/lol/';
+const summonerURL ='v1.4/summoner/by-name/';
+const summoner2URL='v1.4/summoner/';
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -8,8 +15,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res){
-  console.log('here');
+  console.log('here', req.body);
+  request(buildURL(baseURL, 'na/', summonerURL, req.body.summoner, '?'), function(err, response, data){
+    data = JSON.parse(data);
+    console.log(data);
+  });
   res.render('user');
 });
 
 module.exports = router;
+
+
+function buildURL(base, region, url, summoner, option){
+  //console.log(base + url + summoner + option + key);
+  return base + region +url + summoner + option + 'api_key=' + config.key;
+}
