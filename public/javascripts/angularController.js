482ea0505f1cb677073e55fecdd7612be1cdd06e
chrisjyap <chrisjyap@gmail.com>
2015-05-25 18:41:19 -0700

    added basic stat request. implemented rank icon request. button menu bar

--- public/javascripts/angularController.js
+++ public/javascripts/angularController.js
@@ -2,8 +2,10 @@
  * Created by Chris on 5/24/2015.
  */
 
-angular.module('myApp.menu', []).controller('MenuController', function ($scope, $http, summonerID){
+angular.module('myApp.menu', []).controller('MenuController', function ($scope, $http, summonerID, rank){
     $scope.sumID = summonerID;
+    $scope.rank = rank.substring(0, rank.indexOf('.')).replace(/[_]/gi, ' ');
+    if($scope.rank === 'unknown') $scope.rank = 'Unranked';
     console.log($scope.sumID);
 
 });
--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,8 @@ var router = express.Router();
 const patchVer = '5.9.1';
 const summonerURL ='v1.4/summoner/by-name/';
 const summoner2URL='v1.4/summoner/';
+const rankIconURL = 'v2.5/league/by-summoner/';
+const statsURL = 'v1.3/stats/by-summoner/';
 const iconURL = 'http://ddragon.leagueoflegends.com/cdn/'+ patchVer+ '/img/profileicon/';
 
 /* GET users listing. */
@@ -14,19 +16,19 @@ router.get('/', function(req, res, next) {
   //console.log(req.query.summoner);
   var user = req.query.summoner.replace(' ', '').trim();
   console.log('user: ', user);
-  firstRequest(user)
+  idRequest(user)
+      .then(rankIconRequest)
       .then(function(data){
         res.render('user', data);
       })
       .catch(function(err){
         res.render('error', err)
-  });
+      });
 });
-
 
 module.exports = router;
 
-var firstRequest = function (summoner){
+var idRequest = function (summoner){
   var defer = Q.defer();
   request(buildURL('na/', summonerURL, summoner, '?'), function(err, response, data){
     if(err) defer.reject(err);
@@ -38,8 +40,42 @@ var firstRequest = function (summoner){
   return defer.promise;
 };
 
+var rankIconRequest = function(data){
+  var defer = Q.defer();
+  if(!data.id) defer.reject({message: 'No ID found in data.'});
+  else{
+    request(buildURL('na/', rankIconURL, data.id, '/entry?'), function(err, response, requestData){
+      var rankIcon = 'unknown.png';
+      if(response.statusCode === 200){
+        requestData = JSON.parse(requestData);
+        for(var i= 0; i<requestData[data.id].length; i++) {
+          if(requestData[data.id][i]['queue'] === 'RANKED_SOLO_5x5'){
+            rankIcon = requestData[data.id][i].tier + '_' + requestData[data.id][i].entries[0].division + '.png';
+            break;
+          }
+        }
+      }
+      data['rankIcon'] = rankIcon;
+      defer.resolve(data);
+    });
+  }
+  return defer.promise;
+};
+
+var basicStatRequest = function(data){
+  var defer = Q.defer();
+  if(!data.id) defer.reject({message: 'No ID found in data.'});
+  else{
+    request(buildURL('na/', statsURL, data.id, '/summary?'), function(err, response, data){
+      if(err) defer.reject(err);
+      //todo: filter basic stat data
+      defer.resolve(data);
+    });
+  }
+  return defer.promise;
+};
 
 function buildURL(region, url, summoner, option){
-  //console.log('https://na.api.pvp.net/api/lol/' + url + summoner + option);
+  //console.log('https://na.api.pvp.net/api/lol/'+ region + url + summoner + option);
   return 'https://na.api.pvp.net/api/lol/' + region +url + summoner + option + 'api_key=' + config.key;
 }
